fdb90a11eaea039988a9164096f96d33b11ffb6c
taesan <kts030701@gmail.com>
2026-04-28 00:39:14 +0900

    0428

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -1,28 +1,51 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
     void Start()
     {
-        int x;
-        int y;
-        x = 1;
-        y = 2;
-        Debug.Log(x + y);
+        int number = -123;
+        if (number > 0)
+        {
+            Debug.Log("양수");
+        }
+        else if (number < 0)
+        {
+            Debug.Log("음수");
+        }
+        else
+        {
+            Debug.Log(0);
+        }
 
-        long a = 123456789012345;
-        long b = 2345678901;
-        Debug.Log(a * b);
-
-        float c = 1234567890123456;
-        float d = 1234567890000000;
-        Debug.Log((c == d).ToString());
-
-        bool e = (c == d);
-        Debug.Log(e);
-
-        string s = "abcde";
-        string t = "fghij";
-        Debug.Log(s + t);
-    }
-}
+        int year = 2026;
+        if (year % 4 == 0) 
+        {
+             Debug.Log("윤년");
+        }
+        else
+        {
+             Debug.Log("평년");
+        }
+        string cal = "-";
+        int num1 = 2;
+        int num2 = 7;
+        switch (cal)
+        {
+            case "+":
+                Debug.Log(num1 + num2);
+                    break;
+            case "-":
+                Debug.Log(num1 - num2);
+                    break;
+            case "*":
+                Debug.Log(num1 * num2);
+                    break;
+            case "/":
+                Debug.Log(num1 / num2);
+                    break;
+        }
+      
+    }  
+ }
