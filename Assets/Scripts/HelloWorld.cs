7f005156c4f3c72c7ce6209a719560b57cffbdca
taesan <kts030701@gmail.com>
2026-04-30 01:04:49 +0900

    asd

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -5,47 +5,19 @@ public class HelloWorld : MonoBehaviour
 {
     void Start()
     {
-        int number = -123;
-        if (number > 0)
+        int a = 3;
+        int r = 2;
+        int[] n = new int[10];
+        
+        for (int i = 0; i < 10; i++)
         {
-            Debug.Log("양수");
+            n[i] = a;
+            a *= r;
+            Debug.Log(n[i]);
         }
-        else if (number < 0)
-        {
-            Debug.Log("음수");
-        }
-        else
-        {
-            Debug.Log(0);
-        }
+       
 
-        int year = 2026;
-        if (year % 4 == 0) 
-        {
-             Debug.Log("윤년");
-        }
-        else
-        {
-             Debug.Log("평년");
-        }
-        string cal = "-";
-        int num1 = 2;
-        int num2 = 7;
-        switch (cal)
-        {
-            case "+":
-                Debug.Log(num1 + num2);
-                    break;
-            case "-":
-                Debug.Log(num1 - num2);
-                    break;
-            case "*":
-                Debug.Log(num1 * num2);
-                    break;
-            case "/":
-                Debug.Log(num1 / num2);
-                    break;
-        }
-      
+
+
     }  
  }
