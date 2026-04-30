c1814b6617e087f473ecab7f4030131be5882e19
taesan <kts030701@gmail.com>
2026-04-30 17:23:35 +0900

    h

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -1,57 +1,29 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class HelloWorld : MonoBehaviour
 {
     void Start()
     {
-<<<<<<< HEAD
-        int a = 3;
-        int r = 2;
-        int[] n = new int[10];
-        
-        for (int i = 0; i < 10; i++)
-        {
-            n[i] = a;
-            a *= r;
-            Debug.Log(n[i]);
-=======
-        int a = 2;
-        int b = 2;
-        int count = 0;
-        while (a <= 1000)
-        {
-            a *= b;
-            count++;
-        }
-        Debug.Log(count + "번 곱하면 1000을 넘습니다.");
-       
-        for (int i = 1; i < 10; i += 2)
-        {
-            string stars = "";
-            for (int j = 0; j < i;  j++)
-            {
-                stars += "*";
-            }
-            Debug.Log(stars);
->>>>>>> ad4052bffbfe942b88e620962feb9076604f1e94
-        }
-       
+        Dictionary <string, string> 한영사전 = new Dictionary<string, string> ();
+        한영사전.Add("쥐", "mouse");
+        한영사전.Add("잔", "cup");
+        한영사전.Add("손", "hand");
+        한영사전.Add("물", "water");
+        한영사전.Add("발", "foot");
 
+        Dictionary<string, string> 영한사전 = new Dictionary<string, string>();
 
-
-<<<<<<< HEAD
-    }  
-=======
-        for (int c = 2; c < 10; c++)
+        foreach (string key in 한영사전.Keys)
         {
-            for (int d = 1; d < 10; d++)
-            {
-                int answer = c * d;
-                Debug.Log(c + "*" + d + "=" + answer);
-            }
+            영한사전.Add(한영사전[key], key);
         }
 
+        foreach (string key in 영한사전.Keys)
+        {
+            Debug.Log(key + " : " + 영한사전[key]);
+        }
     }
->>>>>>> ad4052bffbfe942b88e620962feb9076604f1e94
+
  }
